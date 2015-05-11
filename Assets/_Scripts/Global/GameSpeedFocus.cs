cce74bd75c6d0cf87cbd078f4014263919d07050
Ramses di Perna <15826@ma-web.nl>
2015-05-11 16:31:37 +0200

    added focus music and linked focus music to focus bar

--- Assets/_Scripts/Global/GameSpeedFocus.cs
+++ Assets/_Scripts/Global/GameSpeedFocus.cs
@@ -10,6 +10,8 @@ public class GameSpeedFocus : MonoBehaviour {
 
 	private bool slidingSlider = false;
 	private float sliderValue = 0;
+	private float sliderSlideSpeed = 0.002f;
+	Slider slider;
 	public float gameSpeed{
 		
 		get{ return _gameSpeed; }
@@ -17,14 +19,13 @@ public class GameSpeedFocus : MonoBehaviour {
 
 	void Start(){
 		TGCConnectionController controller = GameObject.Find("NeuroSkyTGCController").GetComponent<TGCConnectionController>();
-
+		slider = GameObject.Find ("FocusBar").GetComponent<Slider> ();
 		controller.UpdateAttentionEvent += OnUpdateAttention;
 	}
 
 	void OnUpdateAttention(int value){
 		float speedValue = value * 0.01f;
 		SlideSliderToValue(speedValue);
-		_gameSpeed = 1 - (speedValue);
 	}
 
 	void SlideSliderToValue(float value){
@@ -34,18 +35,29 @@ public class GameSpeedFocus : MonoBehaviour {
 
 	void Update(){
 		if(slidingSlider){
-			Slider slider = GameObject.Find ("FocusBar").GetComponent<Slider> ();
 			Debug.Log(Mathf.Abs(slider.value - sliderValue));
 			if(Mathf.Abs(slider.value - sliderValue) > 0.01f){
 				if(slider.value < sliderValue){
-					slider.value += 0.005f;
+					slider.value += sliderSlideSpeed;
 				}else if (slider.value > sliderValue){
-					slider.value -= 0.005f;
+					slider.value -= sliderSlideSpeed;
 
+				}
+
+				if(slider.value > 0.75f){
+					if(GameObject.Find("EffectMusicPlayer").GetComponent<AudioSource>().mute){
+						GetComponent<AudioSource>().volume = 1.15f - slider.value;
+						GameObject.Find("EffectMusicPlayer").GetComponent<AudioSource>().mute = false;
+						GameObject.Find("EffectMusicPlayer").GetComponent<AudioSource>().volume = slider.value - 0.25f;
+					}
+				}else{
+					GetComponent<AudioSource>().volume = 0.8f;
+					GameObject.Find("EffectMusicPlayer").GetComponent<AudioSource>().mute = true;
 				}
 			}else{
 				slidingSlider = false;
 			}
+			_gameSpeed = 1.3f - (slider.value);
 		}
 	}
 }
